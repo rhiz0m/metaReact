5b65b57ac645c563c1457e4cf68368e13e3815b4
Andreas Antonsson <antonsson.andreas@hotmail.com>
2024-11-06 15:02:08 +0100

    module1.Part1 - Exercises

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css"
+import Heading from "./module1/components/heading"
+
+function App() {
+  return (
+    <div className="App">
+      <Heading />
+    </div>
+  )
+}
+
+export default App
---
+++ src/module1/components/heading.js
@@ -0,0 +1,6 @@
+function Heading() {
+  //This is a comment
+  return <h1>This is an h1 heading</h1>
+}
+
+export default Heading
