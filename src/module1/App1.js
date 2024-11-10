a5d06f5c89643a013783a2de4deddff018d29e71
Andreas Antonsson <antonsson.andreas@hotmail.com>
2024-11-10 18:45:15 +0100

    React - Styling JSX elements

--- src/module1/App1.js
+++ src/module1/App1.js
@@ -1,4 +1,5 @@
 import Heading from "./components/heading"
+import styles from "./styles/App.module.css"
 import Bag from "./components/bag"
 import Apples from "./components/apples"
 import Pears from "./components/pears"
@@ -6,7 +7,7 @@ import Pears from "./components/pears"
 // Parent-component
 function App1() {
   return (
-    <div className="App">
+    <div className={styles.appContainer}>
       <Heading firstName="Bob!" />
       <Heading firstName="Sandra!" />
       <Bag background="#ff5533">
