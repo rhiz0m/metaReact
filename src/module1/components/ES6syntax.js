3c80392e2ae86f637ae1bf4433d9ca5fe1f4d25e
Andreas Antonsson <antonsson.andreas@hotmail.com>
2024-11-14 11:19:15 +0100

    Add - more experiments with Arrow Fundctions

--- src/module1/components/ES6syntax.js
+++ src/module1/components/ES6syntax.js
@@ -1,15 +1,20 @@
 // ES6 Syntax
 
-const myItems = (item) => item * 10
+//const myItems = (item) => item * 10
+const myItems = item => item * 10
 console.log(myItems(8)) // 80
 
 const items = [8, 6, 4, 12]
-const createNewArr = items.map((item) => item * 10)
+const createNewArr = items.map((item) => `Map: ${item * 50}`)
 // map method creates a new array, it dosen't mutate the original
 // Use when arr needs to be transformed
 
 console.log(createNewArr) // [ 80, 60, 40, 120 ]
 
-const temporaryArr = items.forEach((item) => item * 10)
+const temporaryArr = items.forEach((item) =>
+  console.log(` ForEach: ${item * 100}`)
+)
 console.log(temporaryArr) // undefined
 // For Each good to show data from ex an API
+
+const test = [4, 2, 6].forEach(item => console.log(` ForEach: ${item * 50}`))
