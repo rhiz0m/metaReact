10c78647335e0655bbde6ae4daaa363716195add
Andreas Antonsson <antonsson.andreas@hotmail.com>
2024-11-19 17:22:23 +0100

    Add - Mouse over event

--- src/module2/components/btn.js
+++ src/module2/components/btn.js
@@ -1,11 +1,21 @@
 function Btn() {
-const eventHandler = () => console.log("Button clicked")
+const eventHandler = () => console.log("Mouse over event!")
 
     return (
         <div>
-            <button onClick={eventHandler}>Click Me!</button>
+            <button onMouseOver={eventHandler}>Hover over me!</button>
         </div>
     )
 }
 
 export default Btn
+
+// function Btn() {
+//   const eventHandler = () => console.log("Button clicked")
+
+//   return (
+//     <div>
+//       <button onClick={eventHandler}>Click Me!</button>
+//     </div>
+//   )
+// }
