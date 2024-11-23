8c09109ba15b6e44f05ce525aab898944570e1a3
Andreas Antonsson <antonsson.andreas@hotmail.com>
2024-11-23 20:55:17 +0100

    Lab on Bird Sounds

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import "./App.css"
 import Homepage from "./module3/Labs_module3/Homepage"
 import Contact from "./module3/Labs_module3/Contact"
-import About from "./module3/Labs_module3/About"
+import LabOnSounds from "./module3/Labs_module3/LabOnSounds"
 import { Route, Link, Routes } from "react-router-dom"
 
 function App() {
-
   return (
     <div>
       <nav>
         <Link to="/" className="nav-item">
           Homepage
         </Link>
-        <Link to="/about" className="nav-item">
-          About
+        <Link to="/labOnSounds" className="nav-item">
+          LabOnSounds
         </Link>
         <Link to="/contact" className="nav-item">
           Contact
@@ -21,7 +20,7 @@ function App() {
       </nav>
       <Routes>
         <Route path="/" element={<Homepage />}></Route>
-        <Route path="/about" element={<About />}></Route>
+        <Route path="/labOnSounds" element={<LabOnSounds />}></Route>
         <Route path="/contact" element={<Contact />}></Route>
       </Routes>
     </div>
